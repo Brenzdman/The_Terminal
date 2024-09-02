1cd673126c8aa519ccd85fbbc084b6d13366a500
Brenzdman <67292734+Brenzdman@users.noreply.github.com>
2024-09-02 11:42:16 -0500

    hard reset

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,16 @@ export default function Home() {
         name="google-site-verification"
         content="hhhgfqTYYPQugAs6cLEnyHM7z1sMIaKjdCIxFysX58M"
       />
+
+      <meta
+        name="description"
+        content="This is a online emulator of a Windows terminal."
+      />
+      <meta
+        name="keywords"
+        content="Windows, Terminal, Command, Prompt, Emulator, Online"
+      />
+
       <DirectoryAtom>
         <ASCII />
         <UserText />
