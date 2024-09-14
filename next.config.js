5673addd6bbc177e60034536b0c139122c914cd3
Brenzdman <67292734+Brenzdman@users.noreply.github.com>
2024-09-14 17:41:14 -0500

    coors update

--- next.config.js
+++ next.config.js
@@ -9,39 +9,54 @@ const nextConfig = {
     return [
       {
         source: "/:path*",
-        headers: nextSafe({
-          isDev,
-          contentSecurityPolicy: {
-            "default-src": ["'self'"],
-            "script-src": [
-              "'self'",
-              "'unsafe-inline'",
-              //   "https://plausible.io/js/script.js", // Analytics
-            ],
-            "img-src": ["'self'", "blob:"],
-            "style-src": ["'self'", "'unsafe-inline'"],
-            "connect-src": [
-              "'self'",
-              //   "https://plausible.io", // Analytics
-            ],
-            // prefetch-src is deprecated
-            // See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/prefetch-src
-            "prefetch-src": false,
+        headers: [
+          ...nextSafe({
+            isDev,
+            contentSecurityPolicy: {
+              "default-src": ["'self'"],
+              "script-src": [
+                "'self'",
+                "'unsafe-inline'",
+                // "https://plausible.io/js/script.js", // Analytics
+              ],
+              "img-src": ["'self'", "blob:"],
+              "style-src": ["'self'", "'unsafe-inline'"],
+              "connect-src": [
+                "'self'",
+                // "https://plausible.io", // Analytics
+              ],
+              // prefetch-src is deprecated
+              // See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/prefetch-src
+              "prefetch-src": false,
+            },
+            // Additional security headers
+            strictTransportSecurity:
+              "max-age=63072000; includeSubDomains; preload",
+            xContentTypeOptions: "nosniff",
+            xFrameOptions: "DENY",
+            xXssProtection: "1; mode=block",
+            referrerPolicy: "no-referrer",
+            permissionsPolicy: {
+              geolocation: ["'none'"],
+              camera: ["'none'"],
+              microphone: ["'none'"],
+              fullscreen: ["'self'"],
+            },
+          }),
+          // CORS headers
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Adjust this to restrict access to specific origins
           },
-          // Additional security headers
-          strictTransportSecurity:
-            "max-age=63072000; includeSubDomains; preload",
-          xContentTypeOptions: "nosniff",
-          xFrameOptions: "DENY",
-          xXssProtection: "1; mode=block",
-          referrerPolicy: "no-referrer",
-          permissionsPolicy: {
-            geolocation: ["'none'"],
-            camera: ["'none'"],
-            microphone: ["'none'"],
-            fullscreen: ["'self'"],
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
           },
-        }),
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-Requested-With, Content-Type, Authorization",
+          },
+        ],
       },
     ];
   },
