33ee53d6145ad21dc22efcc0c5d12091734d9c30
Brenzdman <67292734+Brenzdman@users.noreply.github.com>
2024-09-16 11:00:08 -0500

    num 5

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,28 +1,30 @@
 import { GetServerSideProps } from "next";
 
 const Sitemap = () => {
-  return null;
+  return null; // This page will not render anything
 };
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { res, req } = context;
+export const getServerSideProps: GetServerSideProps = async ({ res, req }) => {
   const baseUrl = `https://${req.headers.host}`;
+
+  // Constructing the sitemap XML
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
-      <loc>${baseUrl}</loc>
+      <loc>${baseUrl}/</loc>
       <lastmod>${new Date().toISOString().split("T")[0]}</lastmod>
       <changefreq>weekly</changefreq>
       <priority>1.0</priority>
     </url>
   </urlset>`;
 
-  res.setHeader("Content-Type", "text/xml");
+  // Setting response headers
+  res.setHeader("Content-Type", "application/xml");
   res.write(sitemap);
   res.end();
 
   return {
-    props: {},
+    props: {}, // No props needed
   };
 };
 
