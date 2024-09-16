9317255307604defc6519d3dc1bd65564b2d4a9b
Brenzdman <67292734+Brenzdman@users.noreply.github.com>
2024-09-16 11:06:22 -0500

    google fix attempt

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Head from "next/head";
 import AccessBox from "@/components/AccessBox";
 import ASCII from "@/components/ASCII";
 import { DirectoryAtom } from "@/components/DirectoryAtom";
@@ -12,19 +13,21 @@ import React from "react";
 export default function Home() {
   return (
     <div>
-      <meta
-        name="google-site-verification"
-        content="hhhgfqTYYPQugAs6cLEnyHM7z1sMIaKjdCIxFysX58M"
-      />
-
-      <meta
-        name="description"
-        content="This is a online emulator of a Windows terminal that will have secrets implemented in the future."
-      />
-      <meta
-        name="keywords"
-        content="Windows, Terminal, Command, Prompt, Emulator, Online"
-      />
+      <Head>
+        <link rel="canonical" href="https://cmdterminal.vercel.app" />
+        <meta
+          name="google-site-verification"
+          content="hhhgfqTYYPQugAs6cLEnyHM7z1sMIaKjdCIxFysX58M"
+        />
+        <meta
+          name="description"
+          content="This is an online emulator of a Windows terminal that will have secrets implemented in the future."
+        />
+        <meta
+          name="keywords"
+          content="Windows, Terminal, Command, Prompt, Emulator, Online"
+        />
+      </Head>
 
       <DirectoryAtom>
         <AccessProvider>
