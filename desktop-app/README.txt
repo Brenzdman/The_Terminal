b065be6a8f602aedb0b5b41a8e205e0b0bbff8d6
Brenzdman <67292734+Brenzdman@users.noreply.github.com>
2024-09-27 11:16:39 -0500

    fixed download

--- desktop-app/README.txt
+++ desktop-app/README.txt
@@ -1,4 +1,4 @@
 // To make exe:
-pkg --debug desktop-app/script.js --output desktop-app/The_Terminal.exe --targets node16-win-x64
+pkg --debug desktop-app/script.js --output desktop-app/app/The_Terminal.exe --targets node16-win-x64
 // To zip
 node desktop-app/package.js
--- desktop-app/script.js
+++ desktop-app/script.js
@@ -1,8 +1,16 @@
 const { exec } = require("child_process");
+const readline = require("readline");
+const fs = require("fs");
+const path = require("path");
 
 // Function to minimize or hide the window (if needed)
 console.log("The Terminal that runs The_Terminal...");
-minimizeWindow();
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 function minimizeWindow() {
   exec(
     "powershell -command \"$wshell = New-Object -ComObject wscript.shell; $wshell.AppActivate((Get-Process -Id $PID).MainWindowTitle); $wshell.SendKeys('% n')\"",
@@ -14,20 +22,90 @@ function minimizeWindow() {
   );
 }
 
-// Install Electron
-const child = exec("npm install electron", { stdio: "inherit" });
+// Function to prompt the user to install npm manually
+function promptUserToInstallNpm() {
+  console.log("node not found.");
+  console.log(
+    "Please install node by following the instructions at `https://nodejs.org/en/download/package-manager`"
+  );
 
-child.on("close", (code) => {
-  // Start the app
-  const command = "cd app && npm start";
-  exec(command, (error, stdout, stderr) => {
+  rl.question("Press Enter after installing npm...", () => {
+    checkForElectron();
+  });
+}
+
+function runCMD() {
+  rl.question("", (cmd) => {
+    const installProcess = exec(cmd);
+
+    installProcess.stdout.on("data", (data) => {
+      console.log(data);
+    });
+
+    installProcess.stderr.on("data", (data) => {
+      console.error(data);
+    });
+
+    installProcess.on("close", (code) => {
+      if (
+        fs.existsSync(path.join(__dirname, "node_modules", ".bin", "electron"))
+      ) {
+        rl.close();
+        checkForElectron();
+      } else {
+        runCMD();
+      }
+    });
+  });
+}
+
+// Function to prompt the user to install Electron manually
+function promptUserForElectronInstallation() {
+  console.log("Electron not found");
+  console.log("Please install Electron by running `npm install electron`");
+  console.log("");
+
+  runCMD();
+}
+
+// Function to start the app with Electron
+function startApp() {
+  const command = `npm start`;
+  const appProcess = exec(command, (error) => {
     if (error) {
-      console.error(`Error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.error(`Stderr: ${stderr}`);
-      return;
+      console.error(`Error starting app: ${error.message}`);
+      return false;
     }
   });
+
+  appProcess.on("close", (code) => {
+    console.log(`App process exited with code ${code}`);
+    rl.close();
+    process.exit(0); // Close the terminal
+  });
+
+  return true;
+}
+
+// Function to check for Electron installation
+function checkForElectron() {
+  // if the start cmd works
+  if (!startApp()) {
+    promptUserForElectronInstallation();
+  } else {
+    console.log("Running The_Terminal...");
+    minimizeWindow();
+  }
+}
+
+// Check if npm is installed
+exec("npm -v", (error, stdout, stderr) => {
+  if (error) {
+    console.error(`npm is not installed: ${error.message}`);
+    promptUserToInstallNpm();
+    return;
+  }
+
+  console.log(`npm version: ${stdout}`);
+  checkForElectron();
 });
